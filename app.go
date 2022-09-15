4090652b831082d562046b6b2fa50a7746dc59eb
Clive Glew <cliveglew@hotmail.com>
2022-09-14 21:08:53 -0300

    Update golang to 1.19 and other bits

--- app.go
+++ app.go
@@ -1,49 +1,48 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"log"
+	"context"
 	"fmt"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"net/http"
-    "context"
-    "time"
-    "strconv"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"strconv"
+	"time"
 )
 
 type App struct {
-	Router *mux.Router
-	Mongo  *mongo.Client
-    NotificationURI string
-    MailURI string
-    DocumentLimit int64
+	Router          *mux.Router
+	Mongo           *mongo.Client
+	NotificationURI string
+	MailURI         string
+	DocumentLimit   int64
 }
 
 func (a *App) Initialize(connectionString, NotificationURI, MailURI, DocumentLimit string) {
 
 	a.Router = mux.NewRouter()
 	a.initializeRoutes()
-    a.NotificationURI = NotificationURI
-    a.MailURI = MailURI
-    var err error
-    var iLimit int64
+	a.NotificationURI = NotificationURI
+	a.MailURI = MailURI
+	var err error
+	var iLimit int64
 
-    iLimit, err = strconv.ParseInt(DocumentLimit, 10, 64)
-    if err != nil {
-        log.Fatal(err)
-    }
-    a.DocumentLimit = iLimit
+	iLimit, err = strconv.ParseInt(DocumentLimit, 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.DocumentLimit = iLimit
 
-    clientOptions := options.Client().ApplyURI(connectionString)
-    ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-    a.Mongo, err = mongo.Connect(ctx, clientOptions)
+	clientOptions := options.Client().ApplyURI(connectionString)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	a.Mongo, err = mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 }
-
 
 func (a *App) Run(addr string) {
 	log.Print(fmt.Sprintf("Server running on port [%s]", addr))
---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/cliveyg/poptqpe-messages
+
+go 1.19
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/mux v1.8.0
+	github.com/jarcoal/httpmock v1.2.0
+	github.com/joho/godotenv v1.4.0
+	github.com/xeipuuv/gojsonschema v1.2.0
+	go.mongodb.org/mongo-driver v1.10.2
+)
+
+require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
